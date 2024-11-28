6a0b5001fab455ab244962abeaa119a709aee35d
Austin Nazworth <austin.nazworth@gmail.com>
2024-11-28 14:28:19 -0500

    Add background

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,10 +2,25 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.color_scheme = "Gruvbox Dark (Gogh)"
+config.color_scheme = "Gruvbox Material (Gogh)"
 
 config.font_size = 14
 
-config.window_background_opacity = 0.94
+config.window_background_opacity = 1
+
+config.background = {
+	{
+		source = {
+			File = wezterm.config_dir .. "/.dotfiles/wallpapers/curry.jpeg",
+		},
+		opacity = 1,
+		vertical_align = "Middle",
+		hsb = {
+			hue = 1,
+			saturation = 1,
+			brightness = 0.02,
+		},
+	},
+}
 
 return config
