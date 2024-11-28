ddd67d98706a3883639ddff7aaff79e08076329d
Austin Nazworth <austin.nazworth@gmail.com>
2024-11-27 20:27:27 -0500

    Change to gruvbox

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,10 +2,10 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.color_scheme = "Catppuccin Macchiato"
+config.color_scheme = "Gruvbox Dark (Gogh)"
 
 config.font_size = 14
 
-config.window_background_opacity = 1
+config.window_background_opacity = 0.94
 
 return config
